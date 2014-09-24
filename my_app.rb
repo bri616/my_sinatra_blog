316ea74fd469df73dee8b5071736cbc717a31d60
Bri Dotson <bridotson@gmail.com>
2014-09-24 10:44:52 -0700

    add about-me

--- my_app.rb
+++ my_app.rb
@@ -4,4 +4,8 @@ class MyApp < Sinatra::Base
   get "/" do #defining a place to go - this would be the homepage
     erb :index #erb looks for a file and reads it
   end
+  get "/about-me" do
+    "All about me!!"
+  end
+
 end
